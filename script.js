68457ac8d64f1d02564e5dff749a85201fa24b92
Gavin <gavin.h81@gmail.com>
2021-10-08 16:27:52 -0400

    Add reset and grid resize features

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
 
-for (let i = 0; i < 256; i++) {
-  let createDiv = document.createElement("div");
+function changeGridSize() {
+  let gridSize = prompt("Enter a number to change the grid size!");
 
-  createDiv.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "black";
-  });
+  while (gridSize < 0 || gridSize > 100 || !gridSize) {
+    gridSize = prompt("Enter a number from 0 to 100!");
+  }
 
-  container.appendChild(createDiv);
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  makeGrid(gridSize);
 }
+
+function makeGrid(number = 16) {
+  const container = document.querySelector(".container");
+  const squarePower = Math.pow(number, 2);
+
+  container.style.setProperty("grid-template-rows", `repeat(${number}, 1fr)`);
+  container.style.setProperty(
+    "grid-template-columns",
+    `repeat(${number}, 1fr)`
+  );
+
+  for (let i = 0; i < squarePower; i++) {
+    let createDiv = document.createElement("div");
+
+    createDiv.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+
+    container.appendChild(createDiv);
+  }
+}
+
+// Load Page
+makeGrid();
+
+// Event Listeners
+button.addEventListener("click", changeGridSize);
